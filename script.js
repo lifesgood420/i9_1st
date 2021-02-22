2074ea8129d3dfd22d5d7d940a677c10d4c0b6a6
lifesgood420 <78197619+lifesgood420@users.noreply.github.com>
2021-02-22 02:34:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function fill_in_empty()
+{
+	document.getElementById("not-found").innerHTML = "<ul><u>A WikiNow jelenleg nem tartalmaz ilyen szócikket.</u> <li class='li-clear'>Küldd el csapatunknak ha van valamiféle információd.</li> <li class='li-clear'>Email : support@wikinow.com</li>  <li class='li-clear'>T.Sz. : +36 xx xxx-xxx</li> </ul>";
+}
